987f913be55bef9580db74eaaa5505173046a31e
Amri Mufti <amrimuvti@gmail.com>
2022-11-26 00:38:41 +0700

    style: edit back to top button

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import styles from '../styles/styles.module.css'
 
 export default function BackToTop() {
     const [toTopButton, setToTopButton] = useState(false)
@@ -15,7 +16,7 @@ export default function BackToTop() {
 
     return (
         <button
-        className={`flex justify-center items-center fixed bottom-[20px] right-[25px] rounded-full pointer-cursor dark:bg-black shadow-lime-400/20 shadow-lg bg-white opacity-50 hover:opacity-70 ring ring-4 ring-lime-500  transition-[opacity, transform] duration-300 ease-in-out ${toTopButton ? 'rotate-0': 'rotate-180'}`}
+        className={`flex justify-center items-center fixed bottom-[20px] right-[25px] pointer-cursor dark:bg-black shadow-lime-400/20 shadow-lg bg-white opacity-50 hover:opacity-70 transition-[opacity, transform] duration-300 ease-in-out ${styles.handDrawnBorderCircle} ${toTopButton ? 'rotate-0': 'rotate-180'}`}
         onClick={() =>
             window.scrollTo({
             top: toTopButton ? 0 : document.body.scrollHeight,
@@ -24,7 +25,7 @@ export default function BackToTop() {
         }
         aria-label='Back to top'
         >
-            <i className="bi bi-chevron-double-up mx-[15px] my-[10px] text-lime-500" style={{ fontSize:18 }}></i>
+            <i className="bi bi-chevron-up mx-[11px] my-[6px] text-lime-500" style={{ fontSize:18, WebkitTextStroke:3 }}></i>
         </button>
     )
 }
