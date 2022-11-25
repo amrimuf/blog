3dccd9637734dc5b3566432b38ae8bd518d3044d
Amri Mufti <amrimuvti@gmail.com>
2022-11-25 20:46:23 +0700

    style: change post category

--- src/components/PostMetaTitle.tsx
+++ src/components/PostMetaTitle.tsx
@@ -14,12 +14,12 @@ export default function PostMetaTitle({ category, title, date, center, slug, fea
   const dateMoment = moment(date).format("MMM DD, YYYY")
   return (
     <div className={`${center ? 'flex w-10/12 md:w-full flex-col items-center mb-6' : ''}`}>
-      <div className="flex items-center text-neutral-600 gap-4 dark:text-neutral-400">
-        <div className="lowercase">
+      <div className="flex items-center gap-4">
+        <div className={`text-neutral-700 ${center ? 'text-neutral-600 dark:text-neutral-400 capitalize' : 'text-neutral-700 text-lg bg-lime-400 absolute top-5 rotate-3 right-5 px-2'}`}>
           {category}
         </div>
-        <span className='text-lime-500 text-xl font-extrabold'>{featured ? <p>&#9885;</p> : <p className='text-[30px]'>&bull;</p>}</span>
-        <div>
+        <span className='text-lime-500 font-extrabold'>{featured ? <p>&#9670;</p> : <p>&#9671;</p>}</span>
+        <div className='text-neutral-600 dark:text-neutral-400'>
           {dateMoment}
         </div>
       </div>
