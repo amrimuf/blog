29e1f594ca5a5e5da81bf26d69833208ad026d40
Amri Mufti <amrimuvti@gmail.com>
2023-01-05 20:34:10 +0700

    fix: og:image info

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -51,7 +51,10 @@ export default function Seo(props: SeoProps) {
         <meta property='og:site_name' content={meta.siteName} />
         <meta property='og:description' content={meta.description} />
         <meta property='og:title' content={meta.title} />
+        <meta property='og:image' content={meta.image} />
         <meta property='og:image:url' content={meta.image} />
+        <meta property='og:image:secure_url' content={meta.image} />
+        <meta property="og:image:type" content="image/png" /> 
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         {/* Twitter */}
