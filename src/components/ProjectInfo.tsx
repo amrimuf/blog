dd625af61b111ec0131bfb90de24e62baad998ad
Amri Mufti <amrimuvti@gmail.com>
2023-01-07 16:42:08 +0700

    style: fix arrow position

--- src/components/ProjectInfo.tsx
+++ src/components/ProjectInfo.tsx
@@ -1,16 +1,15 @@
 import { BsBoxArrowUpRight } from 'react-icons/bs'
 
+import styles from '@/styles/styles.module.css'
 import { Project } from "@/lib/types";
 
 export default function ProjectInfo({...projectInfo}: Project) {
     return (
         <div className='w-full md:w-10/12 flex flex-col space-y-2 py-6'>
-            <div className="flex flex-row gap-4 items-start">
-                <h3>{projectInfo.title}</h3>
-                {projectInfo.post || projectInfo.nolink ?  
-                <BsBoxArrowUpRight className='text-lg' />
-                : ''}
-            </div>
+            <h3>{projectInfo.title}</h3>
+            {projectInfo.post || projectInfo.nolink ?  
+            <BsBoxArrowUpRight className='text-base sm:text-lg absolute right-5 top-5 ml-4' />
+            : ''}
             <hr className="border-black/20 w-full mx-auto mt-10 md:hidden dark:border-white/20" />
             <p>{projectInfo.description}</p>
             <div className="flex flex-wrap justify-start gap-2">
