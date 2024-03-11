6d0f30771292a522bf2a2b8c92ce79b0dc98f795
Amri Mufti <amrimuvti@gmail.com>
2024-03-11 20:38:05 +0700

    style: change blog topics to justify center

--- src/components/PostMetaTitle.tsx
+++ src/components/PostMetaTitle.tsx
@@ -4,6 +4,7 @@ import { PostMetaTitleType } from '@/lib/types'
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { deploymentURL } from '@/constant/env';
+import clsx from 'clsx';
 
 export default function PostMetaTitle({ ...postMeta }: PostMetaTitleType) {
   const [wordCount, setWordCount] = useState<number>(0);
@@ -34,7 +35,7 @@ useEffect(() => {
           <span className='text-lime-500 font-extrabold'>{postMeta.featured ? <span>&#10008;</span> : <span>&#10007;</span>}</span>
           <p><span className={postMeta.center ? 'italic' : 'hidden'}>Updated: </span>{moment(postMeta.date).format("MMM DD, YYYY")}</p>
         </div>
-        <div className="flex flex-wrap justify-start gap-2 mt-2"  data-fade='0'>
+        <div className={clsx(postMeta.center ? "justify-center" : "justify-start", "flex flex-wrap  gap-2 mt-2")}  data-fade='0'>
           {postMeta.topics.map((t:{name:string,slug:string},index:number) => 
           <Link
               href={`${deploymentURL + '/blog/topics/' + t.slug}`}
